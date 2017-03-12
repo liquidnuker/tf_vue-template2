77457870a45c8371ef69bba757dbda3f1a884b29
administrator <liquidnuker@users.noreply.github.com>
2017-03-12 09:24:46 -0800

    store initialized

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import component1 from './component1.vue'
+import store from './store.vue'
 
 new Vue({
   el: '#app-template',
-  render: h => h(component1)
+  render: h => h(store)
 });
 
 console.log("fgfgg");
--- src/store.vue
+++ src/store.vue
@@ -5,6 +5,7 @@
     <h2>app-component-h2</h2>
   </div>
 </template>
+
 <script>
   export default {
     name: 'app-component',
@@ -15,13 +16,3 @@
     }
   }
 </script>
-<style>
-  #app-component {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
